f26b83d54fe0872f27375a09ec1813501baa50a4
svyatlo <svyatlo@list.ru>
2018-09-29 00:26:40 +0300

    The second version

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function getZerosCount(number) {
   // your implementation
   let numberOfZeros = 0;
 
-  while(number > 0) {
+  while(number > 1) {
     number = Math.floor(number / 5);
     numberOfZeros += number;
   }
